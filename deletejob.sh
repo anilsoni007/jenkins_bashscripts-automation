423c8da06afc197c5008e0a90cbf44fa45c1ec2f
Anil_Soni007 <115683735+anilsoni007@users.noreply.github.com>
2023-01-17 12:09:57 +0530

    Update deletejob.sh

--- deletejob.sh
+++ deletejob.sh
@@ -2,4 +2,4 @@ echo "==================================="
 echo "please provide the job name to delete: "
 read job
 echo "Deleting $job "
-java -jar jenkins-cli.jar -s http://13.232.32.227:8080/ -auth jenkins:117657a3a3b07e79ab375b6295fe7f75b2 -webSocket delete-job $job
+java -jar jenkins-cli.jar -s http://13.1.1.1:8080/ -auth jenkins:<jenkins_token> -webSocket delete-job $job
