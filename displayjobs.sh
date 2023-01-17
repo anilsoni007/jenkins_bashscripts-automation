aa9cd184db520ef689a1bf78c47ad0ca82e29eb2
Anil_Soni007 <115683735+anilsoni007@users.noreply.github.com>
2023-01-17 12:10:14 +0530

    Update displayjobs.sh

--- displayjobs.sh
+++ displayjobs.sh
@@ -1,3 +1,3 @@
 echo "Displayig the Jenkins jobs"
 echo "==================================="
-java -jar jenkins-cli.jar -s http://13.232.32.227:8080/ -auth jenkins:117657a3a3b07e79ab375b6295fe7f75b2 -webSocket list-jobs
+java -jar jenkins-cli.jar -s http://13.1.1.1:8080/ -auth jenkins:<jenkins_token> -webSocket list-jobs
