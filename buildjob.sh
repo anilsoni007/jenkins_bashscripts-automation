9e6457aa2c9ddbd8fc52a4e7dab07e444b035d0a
Anil_Soni007 <115683735+anilsoni007@users.noreply.github.com>
2023-01-17 12:09:37 +0530

    Update buildjob.sh

--- buildjob.sh
+++ buildjob.sh
@@ -3,4 +3,4 @@ echo "==================================="
 echo "please provide the job name to trigger build: "
 read job
 echo "Triggering build of $job "
-java -jar jenkins-cli.jar -s http://13.232.32.227:8080/ -auth jenkins:117657a3a3b07e79ab375b6295fe7f75b2 -webSocket build $job
+java -jar jenkins-cli.jar -s http://13.1.1.1:8080/ -auth jenkins:<jenkins_token> -webSocket build $job
